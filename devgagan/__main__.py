0bdf0a67a03101a636974f2c283e9c8730a90438
linksTR <102537035+linksTR@users.noreply.github.com>
2025-05-31 01:05:23 +0300

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -41,7 +41,7 @@ async def devggn_boot():
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
 👨‍💻 Author: Adarsh
-📬 Telegram: https://t.me/Contact_xbot
+📬 Telegram: https://t.me/denujke
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
